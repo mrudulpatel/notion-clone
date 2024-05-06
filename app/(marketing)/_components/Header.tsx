a40bc5d45274b1f5383d3a2570f0d061b04125cb
Mrudul Patel <84196599+mrudulpatel@users.noreply.github.com>
2024-05-06 10:48:16 +0000

    implemented clerk auth and convex database

--- app/(marketing)/_components/Header.tsx
+++ app/(marketing)/_components/Header.tsx
@@ -1,9 +1,14 @@
 "use client";
+import { Spinner } from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +19,26 @@ const Header = () => {
         Motion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Motion <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex justify-center items-center">
+          <Spinner size={"lg"} />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Motion <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Motion Free!
+            <ArrowRight className="h-4 w-4" />
+          </Button>
+          </SignInButton>
+      )}
     </div>
   );
 };
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,9 +4,14 @@ import { useScrollTop } from "@/hooks/use_scroll_top";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/Spinner";
+import Link from "next/link";
 const Navbar = () => {
   const scrolled = useScrollTop();
-
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div
       className={cn(
@@ -16,6 +21,27 @@ const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={"sm"}>Get Motion Free!</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <UserButton afterSignOutUrl="/" />
+            <Button size={"sm"}>
+              <Link href="/documents">Enter Motion</Link>
+            </Button>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
