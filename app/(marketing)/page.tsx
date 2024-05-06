011b9b9bb2b80c8d0f467bd5a90520eef9ad5c56
Mrudul Patel <mrudulpatel04@gmail.com>
2024-05-06 14:52:53 +0530

    first commit

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "./_components/Footer";
+import Header from "./_components/Header";
+import Heroes from "./_components/Heroes";
+
+export default function MarketingPage() {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Header />
+        <Heroes />
+      </div>
+      <Footer />
+    </div>
+  );
+}
