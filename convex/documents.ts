25fcebd3632509d317e2f9a95acbaa2396236149
Mrudul Patel <mrudulpatel04@gmail.com>
2024-05-09 15:36:14 +0530

    Toolbar (icon ,title)

--- convex/documents.ts
+++ convex/documents.ts
@@ -255,3 +255,22 @@ export const update = mutation({
     return document;
   },
 });
+
+export const removeIcon = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not Authenticated");
+
+    const userId = identity.subject;
+
+    const existingDoc = await ctx.db.get(args.id);
+
+    if (!existingDoc) throw new Error("Document not found");
+
+    if (existingDoc.userId !== userId)
+      throw new Error("You can only delete icons from your own documents");
+    
+    const document = await ctx.db.patch(args.id, {icon: undefined});
+  },
+});
